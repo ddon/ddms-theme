c13c8c485d18a644c504a3dca7c1a26443446f9f
root <root@ddms-dev.greenoak.ee>
2022-05-18 11:28:57 +0000

    Add voice fixes

--- includes/Entities/Voice.class.php
+++ includes/Entities/Voice.class.php
@@ -19,86 +19,120 @@ class Voice extends BaseEntity
         $key= "220ddb56efe64c568ba753e3bb51a569";
         $azure_url = "https://northeurope.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=" . $language;
         // $azure_url = "https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken?language=" . $language;
-        $curl = curl_init();
+
+        // $file structure example 
+        // array(5) { 
+        //     ["name"]=> string(27) "ddms-test-voice-record2.wav"
+        //     ["type"]=> string(11) "audio/x-wav"
+        //     ["tmp_name"]=> string(14) "/tmp/phpyuZoqH"
+        //     ["error"]=> int(0)
+        //     ["size"]=> int(1851436)
+        // } 
 
         if ( empty ($file) ) {
             throw new Exception( "File is required" );
         } else {
-            // array(5) { 
-            //     ["name"]=> string(27) "ddms-test-voice-record2.wav"
-            //     ["type"]=> string(11) "audio/x-wav"
-            //     ["tmp_name"]=> string(14) "/tmp/phpyuZoqH"
-            //     ["error"]=> int(0)
-            //     ["size"]=> int(1851436)
-            // } 
+            $supported_types = ["audio/x-wav", "audio/wav", "audio/ogg"];
 
-            if ( $file["type"] !== "audio/x-wav" ) {
-                throw new Exception( "Unsupported file type. Accept audio/x-wav only" );
+            if ( ! in_array( $file["type"], $supported_types) ) {
+                throw new Exception( "Unsupported file type. wav and ogg audio files only" );
             }
 
             // Check file size
-            if ($file["size"] > 5000000) {
-                echo "Sorry, your file more than 5MB";
+            if ( $file["size"] > 5000000 ) {
+                throw new Exception( "Sorry, your file more than 5MB" );
             }
-
         }
 
-        $date = date("Y-m-d-His");
+        $file_type = $file["type"];
+        $file_ext = '';
 
-        if ( empty ($dock) ) {
+        // trying to generate file extension dinamicaly, based on $file_type
+        if ( ! empty ($file_type) && strpos($file_type, "audio/") !== false) {
+            $file_ext = str_replace("audio/","" , $file_type);
+        }
+        
+        // trying to generate directory name to sort audio files.
+        $dock_names = [ 15 => 'dock34', 16 => 'dock2', 17 => 'dock3'];
+        if ( empty ( $dock ) || ! array_key_exists( $dock, $dock_names ) ) {
             $dock = "dockNull";
-        } 
-
-        $target_dir = get_template_directory() . "/voice-records/" . $dock . "/";
-        $target_filename = $date . "-" . $dock . "-audio";
-        $target_file = $target_dir . $target_filename . ".wav";
-
-        $audioFile = '';
-
-        if ( move_uploaded_file($file["tmp_name"], $target_file) ) {
-            // echo "The file ". htmlspecialchars( basename( $file["name"])). " has been uploaded.";
-            $audioFile = $target_file;
         } else {
-            throw new Exception( "Sorry, there was an error uploading your file." );
+            $dock = $dock_names[$dock];
+        }
+     
+        //cunstructing new file name and path
+        $date = date("Y-m-d-His");
+        $target_dir = get_template_directory() . "/voice-records/" . $dock . "/";
+        $target_filename = $date . "-" . $dock;
+        $target_file_path = $target_dir . $target_filename . ".$file_ext";
+
+        $temp_file_path = $file["tmp_name"];
+
+        // error_log(var_export($temp_file_path, true));
+        
+        $original_audio = '';
+
+        //moving temporary file to a new location, converting it to correct format suitable for azure
+        if ( move_uploaded_file( $temp_file_path, $target_file_path ) ) {
+            $original_audio = $target_file_path;
+
+            $converted_audio_filename = $date . "-" . $dock . "-FFMPEG.wav";
+            $converted_audio = $target_dir . $converted_audio_filename;
+            exec( "ffmpeg -i $original_audio $converted_audio" );
+
+            //removig original file if converted ffpmeg exists
+            if ( file_exists( $converted_audio ) ) {
+                // error_log( "\ndeleting original file...\n" );
+                unlink( $original_audio );
+            }
+        } else {
+            throw new Exception( "Sorry, there was an error moving temp file." );
         }
 
-        $file_content = file_get_contents($audioFile);
+        $file_content = file_get_contents( $converted_audio );
 
-        if ( empty ($file_content) ) {
+        if ( empty ( $file_content ) ) {
             throw new Exception( "Couldn't read the file. No such file or directory." );
         }
-
+    
+        //sending file content to azure for speech to text transcription
         $header = [
             "Ocp-Apim-Subscription-Key: $key",
-            "Content-Type: audio/wav",
+            "Content-Type: $file_type",
             "Accept: application/json"
         ];
 
-        curl_setopt($curl, CURLOPT_URL, $azure_url);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $file_content);
+        $curl = curl_init();
+        curl_setopt( $curl, CURLOPT_URL, $azure_url );
+        curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, 'POST' );
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, $header );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_POSTFIELDS, $file_content );
 
         $response = curl_exec($curl);
         curl_close($curl);
 
-        // error_log($response);
-
+        // analyzing response from azure
         if (! empty ($response) ) {
-            $voice_json = json_decode($response);
-            $voice_text = $voice_json->DisplayText;
 
-            // $txt = "Date: $date\nAudio File: $target_filename\nText: $voice_text\nDock: $dock\n\n";
+            $json_response = json_decode($response);
+            $voice_text = $json_response->DisplayText;
+
+            // saving original azure response to log file
             $log_file = fopen( $target_dir . $target_filename . ".json", 'w' );
-
             fwrite($log_file, $response);
             fclose($log_file);
+
+            // error if no transcription text found in the response from azure
+            if ( empty( $voice_text ) ) {
+                throw new Exception( $response );
+            }
+
+            return $json_response;
 
         } else {
             throw new Exception( "Couldn't get transcription!" );
         }
-        
-        return json_decode($response);
+
     }
 }
